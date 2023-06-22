f000dd873761a27577e4ed41e02fcc320ee95184
WijiKurniawan <wijikurniawan121@gmail.com>
2023-06-23 00:56:31 +0700

    tambah dan backup db

--- dbb.php
+++ dbb.php
@@ -1,7 +1,7 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("database-1.ce3kx0ohe4c0.us-east-1.rds.amazonaws.com","admin","12345678","loginsystem");
+    $con = mysqli_connect("localhost","root","","loginsystem");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
